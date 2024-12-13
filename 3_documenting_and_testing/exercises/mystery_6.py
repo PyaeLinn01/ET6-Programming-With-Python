74bb0a90fc36e25d3a0cafeca98147e46fcf0bee
PyaeLinn01 <150521896+PyaeLinn01@users.noreply.github.com>
2024-12-13 22:51:50 +0630

    mystery_6Func

--- 3_documenting_and_testing/exercises/mystery_6.py
+++ 3_documenting_and_testing/exercises/mystery_6.py
@@ -1,4 +1,29 @@
+"""
+mystery_6.py
+
+This module contains the `mystery_6` function, which generates a list of integers starting from `b`.
+
+Functions:
+    mystery_6(a, b): Creates a list of length `a` starting from `b`.
+"""
+
 def mystery_6(a, b):
+    """
+    Generates a list of `a` integers, starting from `b` and incrementing by 1.
+
+    Args:
+        a (int): The number of integers to generate. Must be a non-negative integer.
+        b (int): The starting integer for the list.
+
+    Returns:
+        list: A list of integers of length `a`, starting from `b`.
+
+    Raises:
+        ValueError: If `a` is negative.
+    """
+    if a < 0:
+        raise ValueError("`a` must be a non-negative integer.")
+
     if a == 0:
         return []
 
